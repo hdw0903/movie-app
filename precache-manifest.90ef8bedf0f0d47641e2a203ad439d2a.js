2a1b1832c88325dbfc8160def8c661506ce7807d
Han Dongwon <hdw0903@naver.com>
2020-07-15 21:34:49 +0900

    Updates

---
+++ precache-manifest.90ef8bedf0f0d47641e2a203ad439d2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ab928fc7b57f83a4731a01afdb5a3b",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "942bd3e3c520b05e8b36",
+    "url": "/movie_app/static/css/main.e3d3ce1c.chunk.css"
+  },
+  {
+    "revision": "48c1b3a2eb2215e8c9ee",
+    "url": "/movie_app/static/js/2.e58f7907.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.e58f7907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942bd3e3c520b05e8b36",
+    "url": "/movie_app/static/js/main.7bbf7d99.chunk.js"
+  },
+  {
+    "revision": "b9cb9a6795ceb34c84cd",
+    "url": "/movie_app/static/js/runtime-main.f01f2091.js"
+  }
+]);
